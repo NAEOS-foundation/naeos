dfaebf36e288dff565912320fff7a289b8dccdf7
Bayu <bayu@naeos.dev>
2026-08-27 00:52:58 +0700

    site: fix typecheck error in db/index.ts

--- site/db/index.ts
+++ site/db/index.ts
@@ -2,4 +2,5 @@ import { drizzle } from "drizzle-orm/netlify-db";
 
 import * as schema from "./schema";
 
+// @ts-expect-error netlify-db types expect a connection string but drizzle accepts schema-only for Netlify
 export const db = drizzle({ schema });
