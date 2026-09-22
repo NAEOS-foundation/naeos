5b9fad28f2e3de1d34239d4eef1ee3a14844d4c5
bayu priatno <80336658+bayu911-source@users.noreply.github.com>
2026-09-22 11:09:47 +0700

    fix: restore pipeline policy test import

--- pkg/pipeline/pipeline_test.go
+++ pkg/pipeline/pipeline_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	"github.com/NAEOS-foundation/naeos/internal/governance/review"
+	"github.com/NAEOS-foundation/naeos/internal/governance/policy"
 	"github.com/NAEOS-foundation/naeos/internal/profiling"
 	"github.com/NAEOS-foundation/naeos/internal/schemaregistry"
 	"github.com/NAEOS-foundation/naeos/internal/specification/parser"
